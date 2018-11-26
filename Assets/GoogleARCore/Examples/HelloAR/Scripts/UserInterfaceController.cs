9685a53c0b4fb661d9b60984f7a2186380c29853
Unknown <lessing@live.de>
2018-11-26 22:30:51 +0100

    Circle Token Selection and Controller Adjusment

--- Assets/GoogleARCore/Examples/HelloAR/Scripts/UserInterfaceController.cs
+++ Assets/GoogleARCore/Examples/HelloAR/Scripts/UserInterfaceController.cs
@@ -7,35 +7,63 @@ public class UserInterfaceController : MonoBehaviour {
     // The User Interface for Cross Token Selection
     public GameObject CrossTokenSelection;
 
+    // The User Interface for Circle Token Selection
+    public GameObject CircleTokenSelection;
+
     // The User Interface for Game Field Selection
     public GameObject GameFieldSelection;
 
-    // The Selections
+    // The Current Player (Cross or Circle)
+    private string currentPlayer = "Cross";
+
+    // The selected Token (Cross or Circle)
     private string selectedToken = "";
+
+    // The selected Field on the GameField
     private string selectedField = "";
-    // TODO enum for individual Token and Field ids
 
-    
 
-    public void next()
+    // TODO enum for individual Token and Field ids?!    
+
+    // Deactivates the current Token Selection UI and activates Field Selection Ui
+    public void Next()
     {
         if (selectedToken != "")
         {
-            CrossTokenSelection.SetActive(false);
+            if (currentPlayer == "Cross")
+            {
+                CrossTokenSelection.SetActive(false);
+            }
+            else
+            {
+                CircleTokenSelection.SetActive(false);
+            }
+
             GameFieldSelection.SetActive(true);
-        }        
+        }
     }
 
-    public void prev()
+    // Deactivates Field Selection UI and activates the current Token Selection Ui
+    public void Prev()
     {
         // Reset Selection
         selectedField = "";
         selectedToken = "";
+
         GameFieldSelection.SetActive(false);
-        CrossTokenSelection.SetActive(true);        
+
+        if (currentPlayer == "Cross")
+        {
+            CrossTokenSelection.SetActive(true);
+        }
+        else
+        {
+            CircleTokenSelection.SetActive(true);
+        }
     }
 
-    public void confirm()
+    // Executes the move with the selected Options and changes the current Player
+    public void Confirm()
     {
         if (selectedToken != "" && selectedField != "")
         {
@@ -43,16 +71,32 @@ public class UserInterfaceController : MonoBehaviour {
             // TODO call game function (which disables this controller?!)
             selectedToken = "";
             selectedField = "";
+
+            GameFieldSelection.SetActive(false);
+
+            // Change current Player and UI
+            if (currentPlayer == "Cross")
+            {
+                currentPlayer = "Circle";
+                CircleTokenSelection.SetActive(true);
+            }
+            else
+            {
+                currentPlayer = "Cross";
+                CrossTokenSelection.SetActive(true);
+            }
         }
     }
 
-    public void setToken(string tokenName)
+    // Saves the selected Token
+    public void SetToken(string tokenName)
     {
         selectedToken = tokenName;
         Debug.Log("Selected Token: " + selectedToken);
     }
 
-    public void setField(string fieldName)
+    // Saves the selected Field
+    public void SetField(string fieldName)
     {
         selectedField = fieldName;
         Debug.Log("Selected Field: " + selectedField);
