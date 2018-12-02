fc8348b9b1963180fcc4e1ec1b1be40fb7cefe42
Unknown <lessing@live.de>
2018-12-02 14:32:03 +0100

    placement by gamefield type

--- Assets/TTT3D/Scripts/AugmentedImageVisualizer.cs
+++ Assets/TTT3D/Scripts/AugmentedImageVisualizer.cs
@@ -54,6 +54,15 @@ namespace GoogleARCore.Examples.AugmentedImage
         public GameObject LargeCircle2;
 
         private enum Field { TopLeft, TopMiddle, TopRight, MiddleLeft, Middle, MiddleRight, BottomLeft, BottomMiddle, BottomRight };
+        private enum Token { SmallCross1, MediumCross1, LargeCross1, SmallCircle1, MediumCircle1, LargeCircle1,
+                             SmallCross2, MediumCross2, LargeCross2, SmallCircle2, MediumCircle2, LargeCircle2 };
+
+        private Dictionary<Field, Token> GameField = new Dictionary<Field, Token>()
+        {
+            { Field.TopLeft, Token.SmallCross1 },
+            { Field.Middle, Token.MediumCross2 },
+            { Field.BottomRight, Token.LargeCross1 }
+        };
 
         // Returns a position Vector for the Field on the Gamefield img
         private Vector3 GetPositionVector(Field field, float centerX, float centerY)
@@ -73,6 +82,31 @@ namespace GoogleARCore.Examples.AugmentedImage
                 case Field.BottomRight: return (centerX * Vector3.right) + (centerY * Vector3.back);
 
                 default: return Vector3.zero;
+            }
+        }
+
+        // Returns the Gameobject for a Token enum
+        private GameObject GetTokenObject(Token token)
+        {
+            switch (token)
+            {
+                case Token.SmallCross1: return SmallCross1;
+                case Token.MediumCross1: return MediumCross1;
+                case Token.LargeCross1: return LargeCross1;
+
+                case Token.SmallCircle1: return SmallCircle1;
+                case Token.MediumCircle1: return MediumCircle1;
+                case Token.LargeCircle1: return LargeCircle1;
+
+                case Token.SmallCross2: return SmallCross2;
+                case Token.MediumCross2: return MediumCross2;
+                case Token.LargeCross2: return LargeCross2;
+
+                case Token.SmallCircle2: return SmallCircle2;
+                case Token.MediumCircle2: return MediumCircle2;
+                case Token.LargeCircle2: return LargeCircle2;
+
+                default: return null;
             }
         }
 
@@ -107,26 +141,32 @@ namespace GoogleARCore.Examples.AugmentedImage
                 float centerX = Image.ExtentX / 3;
                 float centerY = Image.ExtentZ / 3;
 
-                SmallCross1.transform.localPosition = GetPositionVector(Field.TopMiddle, centerX, centerY);
-                MediumCross1.transform.localPosition = GetPositionVector(Field.Middle, centerX, centerY);
-                LargeCross1.transform.localPosition = GetPositionVector(Field.BottomMiddle, centerX, centerY);
+                foreach (var placement in GameField)
+                {
+                    GetTokenObject(placement.Value).transform.localPosition = GetPositionVector(placement.Key, centerX, centerY);
+                    GetTokenObject(placement.Value).SetActive(true);
+                }
 
-                SmallCircle1.transform.localPosition = GetPositionVector(Field.BottomLeft, centerX, centerY);
-                MediumCircle1.transform.localPosition = GetPositionVector(Field.MiddleLeft, centerX, centerY);
+                //SmallCross1.transform.localPosition = GetPositionVector(Field.TopLeft, centerX, centerY);
+                //MediumCross1.transform.localPosition = GetPositionVector(Field.Middle, centerX, centerY);
+                //LargeCross1.transform.localPosition = GetPositionVector(Field.BottomRight, centerX, centerY);
 
-                SmallCircle2.transform.localPosition = GetPositionVector(Field.TopLeft, centerX, centerY);
-                MediumCircle2.transform.localPosition = GetPositionVector(Field.BottomRight, centerX, centerY);
+                //SmallCircle1.transform.localPosition = GetPositionVector(Field.BottomLeft, centerX, centerY);
+                //MediumCircle1.transform.localPosition = GetPositionVector(Field.MiddleLeft, centerX, centerY);
+
+                //SmallCircle2.transform.localPosition = GetPositionVector(Field.TopLeft, centerX, centerY);
+                //MediumCircle2.transform.localPosition = GetPositionVector(Field.BottomRight, centerX, centerY);
 
 
-                SmallCross1.SetActive(true);
-                MediumCross1.SetActive(true);
-                LargeCross1.SetActive(true);
+                //SmallCross1.SetActive(true);
+                //MediumCross1.SetActive(true);
+                //LargeCross1.SetActive(true);
 
-                SmallCircle1.SetActive(true);
-                MediumCircle1.SetActive(true);
+                //SmallCircle1.SetActive(true);
+                //MediumCircle1.SetActive(true);
 
-                SmallCircle2.SetActive(true);
-                MediumCircle2.SetActive(true);
+                //SmallCircle2.SetActive(true);
+                //MediumCircle2.SetActive(true);
             }
         }
 
