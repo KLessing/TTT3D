3f1c95fa837bd18104ff36b471c6baa9d56d9a79
Unknown <lessing@live.de>
2018-12-04 22:12:19 +0100

    started btn controller implementation

---
+++ Assets/TTT3D/Scripts/ButtonController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TTT3DTypes;
+
+//using UnityEngine.Experimental.UIElements;
+
+public class ButtonController : MonoBehaviour {
+
+    GameController gameController;
+
+    // Returns Token from Button Name if possible
+    private Token? GetTokenFromBtnName(string btnName)
+    {
+        return (Token)System.Enum.Parse(typeof(Token), btnName.Substring(3));
+    }
+
+    void OnEnable()
+    {
+        Button[] buttons = GetComponentsInChildren<Button>();
+        //GameController gameController; // = GetComponentInParent<GameController>();
+
+        foreach (Button btn in buttons)
+        {
+            Token? btnToken = GetTokenFromBtnName(btn.name);
+            if (btnToken != null)
+            {
+                btn.interactable = gameController.TokenIsCovered((Token)btnToken);
+            }
+
+        }
+    }
+}
